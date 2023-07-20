2a4bbe6d74f70dd19574da0cdc840b3d4cb4b448
Romina <113934683+romipuntel@users.noreply.github.com>
2023-07-20 16:19:35 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+import compression from 'express-compression'
+import CustomError from './utils/errors/CustomError.js'
+import { ErrorsCause, ErrorsMessage, ErrosName } from './utils/errors/errors.enum.js'
+import { errorMiddleware } from './utils/errors/errors.middleware.js'
 import express from 'express'
 import { generateUser } from './utils/mocks.js'
+
 const app = express()
 
 app.get('/api/users', (req, res) => {
@@ -10,6 +15,34 @@ app.get('/api/users', (req, res) => {
   }
   res.json({ users })
 })
+
+app.use(compression({ brotli: { enabled: true, zlib: {} } }))
+
+app.get('/', (req, res) => {
+    let info = 'Primera prueba compresion'
+    for (let i = 0; i < 100000; i++) {
+        info += 'Agregando desde el ciclo for'
+    }
+    res.send(info)
+})
+
+app.post('/products', (req, res) => {
+    CustomError.createCustomError({
+        name: ErrosName.PRODUCT_PRINCIPAL_ERROR_POST,
+        message: ErrorsMessage.PRODUCT_PRINCIPAL_ERROR_POST,
+        cause: ErrorsCause.PRODUCT_PRINCIPAL_ERROR_POST,
+    })
+    
+})
+
+app.get('/products',(req,res)=>{
+    CustomError.createCustomError({
+        name: ErrosName.PRODUCT_PRINCIPAL_ERROR_GET,
+        message: ErrorsMessage.PRODUCT_PRINCIPAL_ERROR_GET,
+        cause: ErrorsCause.PRODUCT_PRINCIPAL_ERROR_GET,
+    })
+})
+
 
 app.listen(8080, () => {
   console.log('Escuchando al puerto 8080')
