7ee08bbe0b31d664de451df47d15d8be4e9ad101
Romina <113934683+romipuntel@users.noreply.github.com>
2023-07-20 16:17:41 -0400

    Add files via upload

---
+++ src/utils/CustomError.js
@@ -0,0 +1,7 @@
+export default class CustomError {
+  static createCustomError({ name, message, cause }) {
+    const newError = new Error(message, { cause })
+    newError.name = name
+    throw newError
+  }
+}
