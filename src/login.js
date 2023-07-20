03ddb68623a18be9878c6b706fdeffdfaae48c9c
Romina <113934683+romipuntel@users.noreply.github.com>
2023-07-20 16:19:50 -0400

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,22 +1,4 @@
-// export const login = (user, password) => {
-//   if (password === null || password === undefined) {
-//     return 'No se ha proporcionado un password'
-//   }
-//   if (user === null) {
-//     return 'No se ha proporcionado un usuario'
-//   }
-//   if (password !== '123') {
-//     return 'Password incorrecta'
-//   }
-//   if (user !== 'coderUser') {
-//     return 'Credenciales incorrectas'
-//   }
-//   if (user === 'coderUser' && password === '123') {
-//     return 'logueado'
-//   }
-// }
 
-//refactorizacion
 
 export const login = (user, password) => {
     if (!password) return 'No se ha proporcionado un password'
