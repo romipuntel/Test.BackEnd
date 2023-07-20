fc743a703fcf0704ae6f226ac089c4ff1f289e6b
Romina <113934683+romipuntel@users.noreply.github.com>
2023-07-20 16:16:46 -0400

    Delete CustomError.js

--- CustomError.js
+++
@@ -1,7 +0,0 @@
-export default class CustomError {
-  static createCustomError({ name, message, cause }) {
-    const newError = new Error(message, { cause })
-    newError.name = name
-    throw newError
-  }
-}
